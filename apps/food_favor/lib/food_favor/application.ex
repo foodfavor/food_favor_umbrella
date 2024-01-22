fc644cc4986926e9ca96afc184dfb2f1f7887a52
Stamates <Stamates@users.noreply.github.com>
2024-01-22 12:12:21 -0500

    Add Sentry

--- apps/food_favor/lib/food_favor/application.ex
+++ apps/food_favor/lib/food_favor/application.ex
@@ -7,6 +7,8 @@ defmodule FoodFavor.Application do
 
   @impl true
   def start(_type, _args) do
+    Logger.add_backend(Sentry.LoggerBackend)
+
     children = [
       FoodFavor.Repo,
       {DNSCluster, query: Application.get_env(:food_favor, :dns_cluster_query) || :ignore},
